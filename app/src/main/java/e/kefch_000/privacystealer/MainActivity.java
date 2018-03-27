65c9d82367e345a02cb1e5e4ae968df92c03d163
kefche <kefche2003@hotmail.com>
2018-03-27 20:07:33 +0300

    Update 2
    Made a successful location getter( to lat&long + city).
    TODO: do it as a background task

--- app/src/main/java/e/kefch_000/privacystealer/MainActivity.java
+++ app/src/main/java/e/kefch_000/privacystealer/MainActivity.java
@@ -1,31 +1,185 @@
 package e.kefch_000.privacystealer;
 
+
 import android.Manifest;
-import android.app.Service;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.Handler;
-import android.os.IBinder;
-import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends Activity
+        implements OnClickListener {
+
+    private static final String TAG = "Debug";
+    private LocationManager locationManager = null;
+    private LocationListener locationListener = null;
+    private Button btnGetLocation = null;
+    private EditText editLocation = null;
+    private ProgressBar pb = null;
+    private Boolean flag = false;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startService(new Intent(this, MyLocationService.class));
+
+
+
+        setRequestedOrientation(ActivityInfo
+                .SCREEN_ORIENTATION_PORTRAIT);
+//could use butterknife, chose ths way because im an idiot
+        pb = findViewById(R.id.progressBar1);
+        pb.setVisibility(View.INVISIBLE);
+
+        editLocation = findViewById(R.id.editTextLocation);
+
+        btnGetLocation = findViewById(R.id.btnLocation);
+        btnGetLocation.setOnClickListener(this);
+
+        locationManager = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
 
     }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onClick(View v) {
+        flag = displayGpsStatus();
+        if (flag) {
+
+            Log.v(TAG, "onClick");
+
+            editLocation.setText(getString(R.string.moveyourdevice_txt));
+
+            pb.setVisibility(View.VISIBLE);
+            locationListener = new MyLocationListener();
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+
+
+                return;
+            }
+            locationManager.requestLocationUpdates(LocationManager
+                    .GPS_PROVIDER, 5000, 5, locationListener);
+
+        } else {
+            alertbox("Gps Status", "Your GPS is: OFF");
+        }
+
+    }
+
+
+    private Boolean displayGpsStatus() {
+        ContentResolver contentResolver = getBaseContext()
+                .getContentResolver();
+
+        boolean gpsStatus = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        return gpsStatus;
+    }
+    protected void alertbox(String title, String mymessage) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Turn your location on")
+                .setCancelable(false)
+                .setTitle("Location Status")
+                .setPositiveButton("Gps On",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Intent myIntent = new Intent(
+                                        Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                                startActivity(myIntent);
+                                dialog.cancel();
+                            }
+                        })
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+    private class MyLocationListener implements LocationListener {
+        @Override
+        public void onLocationChanged(Location loc) {
+
+            editLocation.setText("");
+            pb.setVisibility(View.INVISIBLE);
+            Toast.makeText(getBaseContext(), "Location changed : Lat: " +
+                            loc.getLatitude() + " Lng: " + loc.getLongitude(),
+                    Toast.LENGTH_SHORT).show();
+            String longitude = "Longitude: " + loc.getLongitude();
+            Log.v(TAG, longitude);
+            String latitude = "Latitude: " + loc.getLatitude();
+            Log.v(TAG, latitude);
+
+    /*----------to get City-Name from coordinates ------------- */
+            String cityName = null;
+            Geocoder gcd = new Geocoder(getBaseContext(),
+                    Locale.getDefault());
+            List<Address> addresses;
+            try {
+                addresses = gcd.getFromLocation(loc.getLatitude(), loc
+                        .getLongitude(), 1);
+                if (addresses.size() > 0)
+                    System.out.println(addresses.get(0).getLocality());
+                cityName = addresses.get(0).getLocality();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            String s = longitude + "\n" + latitude +
+                    "\n\nMy Currrent City is: " + cityName;
+            editLocation.setText(s);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            alertbox("Gps Status", "Your GPS is: OFF");
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+
+        @Override
+        public void onStatusChanged(String provider,
+                                    int status, Bundle extras) {
+        }
+    }
+
 
  /*   Location gpslocation = null;
     private static final int GPS_TIME_INTERVAL = 100;
